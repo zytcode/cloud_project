e3441c8f543130e2de23a402eb68c1c5e72c9436
YanTengZhang <zytcode@foxmain.com>
2024-11-01 18:49:01 +0800

    1

---
+++ provider_rest_8001/src/main/java/com/zytcode/ProviderRest8001Application.java
@@ -0,0 +1,13 @@
+package com.zytcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderRest8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderRest8001Application.class, args);
+    }
+
+}
---
+++ provider_rest_8001/src/test/java/com/zytcode/ProviderRest8001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zytcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderRest8001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
